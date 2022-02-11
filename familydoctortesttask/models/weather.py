a6e948ec13636a58d83a38796445dc5840a55681
Vladimir Panferov <v3nseagal@gmail.com>
2022-02-11 16:16:10 +0700

    Add weather models

---
+++ familydoctortesttask/models/weather.py
@@ -0,0 +1,42 @@
+from datetime import date
+
+from pydantic import BaseModel
+
+
+class CountryBase(BaseModel):
+    name: str
+    code: str
+
+
+class Country(CountryBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class CityBase(BaseModel):
+    name: str
+    code: str
+
+
+class City(CityBase):
+    id: int
+    country_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class WeatherForecastBase(BaseModel):
+    date: date
+    weather: int
+
+
+class WeatherForecast(WeatherForecastBase):
+    id: int
+    country_id: int
+    city_id: int
+
+    class Config:
+        orm_mode = True
