ec7b9d70ad561ef41e0af6b22ee0ca38f1662cb3
Vladimir Panferov <v3nseagal@gmail.com>
2022-02-11 12:37:02 +0700

    Add database

---
+++ familydoctortesttask/database.py
@@ -0,0 +1,24 @@
+from pytest import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from settings import settings
+
+
+engine = create_engine(
+    settings.database_url,
+)
+
+Session = sessionmaker(
+    engine,
+    autocommit=False,
+    autoflush=False,
+)
+
+
+def get_session():
+    session = Session()
+    try:
+        yield session
+    except:
+        session.close()
