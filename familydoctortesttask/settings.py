a312f06afa5c5b4b989ab5cc14a677a2bbbb37be
Vladimir Panferov <v3nseagal@gmail.com>
2022-02-14 20:24:34 +0700

    Update settings

--- familydoctortesttask/settings.py
+++ familydoctortesttask/settings.py
@@ -6,7 +6,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     weather_api: str
     weather_key: str
-    weather_time: Optional[str] = "T12:00"
+    weather_time: Optional[str] = "12:00"
 
     database_url: str
 
